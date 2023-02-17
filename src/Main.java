655a35999c62916751eb13da327dbcb7faf533a3
id251 <danyalider178@gmail.com>
2023-02-17 19:56:55 +0300

    comm

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,24 @@
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-        Random random=new Random();
-        LinkedList<Integer>integers=new LinkedList<>();
-        int counter=0;
-        for (int i = 0; i <= 400; i++) {
-            integers.add(random.nextInt(20));
-            counter+=integers.get(i);
-        }
-        for (Integer a:integers) {
-            System.out.println(a);
-        }
-        System.out.println(" summa: "+ counter);
-        HashSet<Integer>integers1=new HashSet<>();
-        integers1.addAll(integers);
-        int counter2=0;
-        for ( Integer s:integers1) {
-            counter2+=s;
-        }
-        System.out.println("summa: "+ counter2);
+        Student student=new Student(17,"Aibek");
+        Student student2=new Student(17,"Aibek");
+        Student student3=new Student(17,"Aibek");
+        Student student4=new Student(17,"Aibek");
+        Student student5=new Student(17,"Aibek");
+        LinkedList<Student>students=new LinkedList<>();
+        students.add(student);
+        students.add(student2);
+        students.add(student3);
+        students.add(student4);
+        students.add(student5);
+        HashSet<Student>students2=new HashSet<>();
+        students2.addAll(students);
+        students2.forEach(System.out::println);
 
 
     }
